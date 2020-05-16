5332346d13e15e362d7dccf9d1d39925504e70eb
Lorenzo Peña <lorinkoz@gmail.com>
2020-05-16 17:08:30 -0400

    First experiment

--- index.js
+++ index.js
@@ -1,19 +1,76 @@
-/**
- * This is the main entrypoint to your Probot app
- * @param {import('probot').Application} app
- */
-module.exports = app => {
-  // Your code here
-  app.log('Yay, the app was loaded!')
+const createScheduler = require("probot-scheduler");
+const moment = require("moment");
 
-  app.on('issues.opened', async context => {
-    const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
-    return context.github.issues.createComment(issueComment)
-  })
+const configPath = "entomologist.yml",
+  checkInterval = 5 * 60 * 1000,
+  defaultConfig = {
+    labels: {
+      unconfirmed: "Status: Requires confirmation",
+      confirmed: "Status: Confirmed",
+      duplicated: "Status: Duplicated",
+    },
+    timeouts: {
+      markUnconfirmed: 5, // minutes
+    },
+  };
 
-  // For more information on building apps:
-  // https://probot.github.io/docs/
-
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
+async function configFromContext(context) {
+  return await context.config(configPath, { ...defaultConfig });
 }
+
+module.exports = (app) => {
+  app.log("Entomologist on duty!");
+  createScheduler(app, {
+    interval: checkInterval,
+  });
+
+  // Periodic housekeeping
+  app.on("schedule.repository", async (context) => {
+    const config = configFromContext(context);
+    const { owner, repo } = context.repo();
+    const result = await context.github.search.issues({
+      q: `no:label" repo:${owner}/${repo}`,
+    });
+
+    await Promise.all(
+      result.data.items.map(async (issue) => {
+        // Marking unconfirmed
+        if (
+          moment(issue.created_at).add(config.timeouts.markUnconfirmed) <
+          moment()
+        ) {
+          app.log(`Marking issue #${issue.number} as unconfirmed.`);
+          context.github.issues.addLabels(
+            issue({
+              labels: [config.labels.unconfirmed],
+            })
+          );
+        }
+      })
+    );
+  });
+
+  // Removing stale labels
+  app.on("issues.labeled", async (context) => {
+    const config = configFromContext(context);
+    if (context.issue.label == config.labels.confirmed) {
+      app.log(
+        `Removing "${config.labels.unconfirmed}" from labels on issue #${context.issue.number}.`
+      );
+      context.github.issues.removeLabels(
+        context.issue({
+          labels: [config.labels.unconfirmed],
+        })
+      );
+    } else if (context.issue.label == config.labels.unconfirmed) {
+      app.log(
+        `Removing "${config.labels.confirmed}" from labels on issue #${context.issue.number}.`
+      );
+      context.github.issues.removeLabels(
+        context.issue({
+          labels: [config.labels.confirmed],
+        })
+      );
+    }
+  });
+};
