b3ab668a892a04f760be212ea3439b1ed399e46b
Lorenzo Peña <lorinkoz@gmail.com>
2020-05-17 07:23:22 -0400

    Refactored all code

--- index.js
+++ index.js
@@ -1,196 +1,159 @@
 const createScheduler = require("probot-scheduler");
+const metadata = require("probot-metadata");
 const moment = require("moment");
 
 const configPath = "ladybug.yml",
-  checkInterval = 5 * 60 * 1000,
-  defaultConfig = {
-    labels: {
-      unconfirmed: "Status: Needs reproducing",
-      confirmed: "Status: Confirmed",
-      duplicated: "Status: Duplicated",
-    },
-    timeouts: {
-      markUnconfirmed: 5, // minutes
-      firstComment: 10 * 60, // minutes (10 hours)
-    },
-  };
+	defaultConfig = {
+		labels: {
+			unconfirmed: "Status: Needs reproducing",
+			confirmed: "Status: Confirmed",
+			duplicated: "Status: Duplicated",
+		},
+		timeouts: {
+			unlabeled: 5, // minutes
+			unassigned: 10 * 60, // minutes (10 hours)
+		},
+	};
 
 module.exports = (app) => {
-  app.log("Ladybug on duty!");
-  createScheduler(app, {
-    interval: checkInterval,
-  });
+	app.log("Ladybug on duty!");
+	createScheduler(app, {
+		interval: 5 * 60 * 1000, // 5 minutes
+	});
 
-  // Periodic housekeeping
-  app.on("schedule.repository", async (context) => {
-    const config = await context.config(configPath, defaultConfig);
-    const { owner, repo } = context.repo();
-    const searchResults = await context.github.search.issues({
-      q: `no:label" repo:${owner}/${repo}`,
-    });
+	app.on("schedule.repository", periodicCheck);
+	app.on("issues.labeled", removePeerLabels);
+	app.on("issue_comment.created", checkDuplicateIssue);
+	app.on("issue_comment.edited", checkDuplicateIssue);
+	app.on("issue_comment.deleted", checkDuplicateIssue);
+	app.on("issue.closed", checkChainClosing);
+	app.on("issue.reopened", checkChainReopening);
 
-    await Promise.all(
-      searchResults.data.items.map(async (issue) => {
-        // Marking unconfirmed
-        if (
-          issue.state == "open" &&
-          moment(issue.created_at).add(config.timeouts.markUnconfirmed) <
-            moment() &&
-          !issue.labels.length
-        ) {
-          issue = context.repo(issue);
-          const { owner, repo, number } = issue;
+	// Tasks of periodic housekeeping
+	async function periodicCheck(context) {
+		const config = await context.config(configPath, defaultConfig);
+		const { owner, repo } = context.repo();
+		const searchResults = await context.github.search.issuesAndPullRequests({
+			q: `no:label" repo:${owner}/${repo}`,
+		});
 
-          app.log(`Marking issue #${issue.number} as unconfirmed.`);
+		async function declareUnconfirmed(issue, context) {
+			const { owner, repo, number } = issue;
+			app.log(`Declaring issue #${issue.number} as unconfirmed.`);
+			context.github.issues.addLabels({
+				owner,
+				repo,
+				number,
+				labels: [config.labels.unconfirmed],
+			});
+		}
 
-          context.github.issues.addLabels({
-            owner,
-            repo,
-            number,
-            labels: [config.labels.unconfirmed],
-          });
-        }
+		async function declareFirstChime(issue, context) {
+			const { owner, repo, number } = issue;
+			app.log(`Chiming on #${issue.number} for the first time.`);
+			context.github.issues.createComment({
+				owner,
+				repo,
+				number,
+				body: "Buzzing in to put this report in your radar.",
+			});
+		}
 
-        // Commenting to draw attention
-        if (
-          issue.state == "open" &&
-          moment(issue.created_at).add(config.timeouts.firstComment) <
-            moment() &&
-          !issue.comments &&
-          !issue.assignee
-        ) {
-          issue = context.repo(issue);
-          const { owner, repo, number } = issue;
+		await Promise.all(
+			searchResults.data.items.map(async (issue) => {
+				issue = context.repo(issue);
+				const unlabeled =
+						issue.state == "open" &&
+						moment(issue.created_at).add(config.timeouts.unlabeled, "minutes") < moment() &&
+						!issue.labels.length,
+					unassigned =
+						issue.state == "open" &&
+						moment(issue.created_at).add(config.timeouts.unassigned, "minutes") < moment() &&
+						!issue.assignee;
 
-          app.log(`Commenting on #${issue.number} for the first time.`);
+				if (unlabeled) {
+					declareUnconfirmed(issue, context);
+				}
+				if (unassigned) {
+					declareFirstChime(issue, context);
+				}
+			})
+		);
+	}
 
-          context.github.issues.createComment({
-            owner,
-            repo,
-            number,
-            body: "Buzzing in to let you know about this report.",
-          });
-        }
-      })
-    );
-  });
+	// Removes peer labels
+	async function removePeerLabels(context) {
+		const peerRegex = /^(.*):/,
+			{ label, issue } = context.payload,
+			match = label.name.match(peerRegex);
 
-  // Removing stale labels
-  app.on("issues.labeled", async (context) => {
-    const config = await context.config(configPath, defaultConfig);
-    app.log(`Checking labeled issue #${context.payload.issue.number}.`);
+		if (match) {
+			const labels = issue.labels.filter((x) => !x.name.match(peerRegex));
+			if (!labels.includes(label.name)) {
+				labels.push(label.name);
+			}
+			if (labels.length) {
+				await context.github.issues.replaceLabels(
+					context.issue({
+						labels,
+					})
+				);
+			}
+		}
+	}
 
-    // Removing 'unconfirmed' if 'confirmed' was added
-    if (
-      context.payload.label.name == config.labels.confirmed &&
-      context.payload.issue.labels.filter(
-        (x) => x.name == config.labels.unconfirmed
-      ).length
-    ) {
-      app.log(
-        `Removing "${config.labels.unconfirmed}" from labels on issue #${context.payload.issue.number}.`
-      );
-      context.github.issues.removeLabel(
-        context.issue({
-          name: config.labels.unconfirmed,
-        })
-      );
-    }
-    // Removing 'confirmed' if 'unconfirmed' was added
-    else if (
-      context.payload.label.name == config.labels.unconfirmed &&
-      context.payload.issue.labels.filter(
-        (x) => x.name == config.labels.confirmed
-      ).length
-    ) {
-      app.log(
-        `Removing "${config.labels.confirmed}" from labels on issue #${context.payload.issue.number}.`
-      );
-      context.github.issues.removeLabel(
-        context.issue({
-          name: config.labels.confirmed,
-        })
-      );
-    }
-  });
+	// Checks created / edited / deleted issues for duplicate marking
+	async function checkDuplicateIssue(context) {
+		const config = await context.config(configPath, defaultConfig),
+			dupRegex = /^Duplicate of #(\d+)/,
+			{ issue, comment, action, changes } = context.payload;
 
-  // Labelling / unlabelling as duplicate
-  app.on(
-    ["issue_comment.created", "issue_comment.edited", "issue_comment.deleted"],
-    async (context) => {
-      const config = await context.config(configPath, defaultConfig);
+		// Mark issue as duplicate
+		async function markDuplicate(duplicateOf) {
+			app.log(`Marking issue #${issue.number} as dup of #${duplicateOf}.`);
+			await metadata(context).set("duplicateOf", duplicateOf);
+			await context.github.issues.replaceLabels(
+				context.issue({
+					labels: [config.labels.duplicated],
+				})
+			);
+		}
 
-      if (context.payload.issue.state == "open") {
-        const eventResults = await context.github.issues.listEvents(
-          context.issue({
-            issue_number: context.payload.issue.number,
-            per_page: 100,
-          })
-        );
+		// Unmark issue as duplicate
+		async function unmarkDuplicate(duplicateOf) {
+			app.log(`Unmarking issue #${issue.number} as dup of #${duplicateOf}.`);
+			await metadata(context).set("duplicateOf", undefined);
+			context.github.issues.removeLabel(
+				context.issue({
+					name: config.labels.duplicated,
+				})
+			);
+		}
 
-        app.log(
-          `Checking comment action on issue #${context.payload.issue.number}.`
-        );
-        const scopedEvents = eventResults.data.filter((x) =>
-          ["marked_as_duplicate", "unmarked_as_duplicate"].includes(x.event)
-        );
-        const targetEvent =
-          scopedEvents.length && scopedEvents.length < 100
-            ? scopedEvents[scopedEvents.length - 1]
-            : null;
+		if (issue.state == "open" && action == "created") {
+			const match = comment.body.match(dupRegex);
+			if (match) {
+				markDuplicate(match[1]);
+			}
+		} else if (issue.state == "open" && action == "edited") {
+			const matchBefore = changes.body && changes.body.from.match(dupRegex),
+				matchAfter = comment.body.match(dupRegex);
+			if (!matchBefore && matchAfter) {
+				markDuplicate(matchAfter[1]);
+			} else if (matchBefore && !matchAfter) {
+				unmarkDuplicate(matchBefore[1]);
+			}
+		} else if (issue.state == "open" && action == "deleted") {
+			const match = comment.body.match(dupRegex);
+			if (match) {
+				unmarkDuplicate(match[1]);
+			}
+		}
+	}
 
-        if (scopedEvents.length >= 100) {
-          app.log(
-            `Not pursuing any action on issue #${context.payload.issue.number}. Too many events to process.`
-          );
-        } else if (!targetEvent) {
-          app.log(
-            `Not pursuing any action on issue #${context.payload.issue.number}. No interesting events.`
-          );
-        }
+	// Checks closed issue for chain closing
+	async function checkChainClosing(context) {}
 
-        // Labelling as duplicate
-        if (
-          targetEvent &&
-          targetEvent.event == "marked_as_duplicate" &&
-          !context.payload.issue.labels.filter(
-            (x) => x.name == config.labels.duplicated
-          ).length
-        ) {
-          app.log(
-            `Labelling issue #${context.payload.issue.number} as duplicate.`
-          );
-          await context.github.issues.replaceLabels(
-            context.issue({
-              labels: [config.labels.duplicated],
-            })
-          );
-        }
-
-        // Unlabelling as duplicate
-        if (
-          targetEvent &&
-          targetEvent.event == "unmarked_as_duplicate" &&
-          context.payload.issue.labels.filter(
-            (x) => x.name == config.labels.duplicated
-          ).length
-        ) {
-          app.log(
-            `Unlabelling issue #${context.payload.issue.number} as duplicate.`
-          );
-          context.github.issues.removeLabel(
-            context.issue({
-              name: config.labels.duplicated,
-            })
-          );
-        }
-      }
-    }
-  );
-
-  // Closing duplicated issues together
-  app.on("issue.closed", async (context) => {});
-
-  // Reopening duplicated issues together
-  app.on("issue.reopened", async (context) => {});
+	// Checks reopened issue for chain reopening
+	async function checkChainReopening(context) {}
 };
