9777db7cfabb39317f9eccdc4c0604f7106be991
Lorenzo Peña <lorinkoz@gmail.com>
2020-05-17 21:45:30 -0400

    Complete rewrite in TypeScript, expanded scope

--- README.md
+++ README.md
@@ -1,27 +1,99 @@
 # Probot Ladybug
 
-> A GitHub App built with [Probot](https://github.com/probot/probot) to triage bug reports
+> A GitHub App built with [Probot](https://github.com/probot/probot) to triages issues
 
-This app is capable of:
+Ladybug has these superpowers:
 
-- Label your unlabelled issues after some time of creation.
-- Comment on your unassigned issues after some time of creation.
-- Follow up on your open issues if they haven't been updated for some time.
-- Track duplicated issues and close them when the referred issue is closed. Reopen them if the referred issue is reopened.
-- Remove peer labels when a label is assigned (peer labels are those that begin with a common prefix like `Something:`, the colon is important)
+- When you have peer labels (e.g. `Status: Reproducing`, `Status: Confirmed`) she removes the other peer labels when one of them is assigned.
+- If you mark an issue as duplicate of another, and don't close it, she will remember this and will chain close and chain reopen the duplicated issues when the reference issue is closed or reopened.
+- You can configure rules for interacting with your issues. This is actually the superpower she's most proud of.
 
-Configuration is expected in `.github/ladybug.yml`. This is the default configuration:
+Ladybug responds to a configuration file located in `.github/ladybug.yml`.
+
+You can turn on/off the management of peer labels via:
 
 ```yml
-labels: # Labels to track status of issues
-  confirmed: "Status: Confirmed"
-  duplicated: "Status: Duplicated"
-  unconfirmed: "Status: Needs reproducing"
-timeouts:
-  unchimed: 10 # 10 hours to comment on issues with no comments and no assignees
-  unfollowed: 48 # 48 hours to follow up on issues without modifications
-  unlabeled: 1 # 1 hour to label unlabeled issues as unconfirmed
+peer_labels: false # default is true
 ```
+
+You can also control the management of duplicated issue via:
+
+```yml
+duplicated_issues: false
+```
+
+The default value for this setting is:
+
+```yml
+duplicated_issues:
+  label: "Status: Duplicated" # Label that will be used to mark / control duplicated issues
+  chain_close: true # Whether to close duplicated issues in chain
+  chain_reopen: true # Whether to reopen duplicated issues in chain
+```
+
+Finally, the rules for interacting with your issues can be configured through a set of conditions and actions.
+Better to learn from an example:
+
+```yml
+scheduled_checks:
+  label_unlabelled: # codename of the rule
+    if_type: issue
+    if_state: open
+    if_created: "1 hour"
+    if_label: no
+    add_labels: "Status: Pending"
+    comment: "Since this has been one hour without labels, I took the liberty to mark as pending"
+  assign_unassigned:
+    if_type: issue
+    if_state: open
+    if_created: "8 hours"
+    if_assignee: no
+    add_assignees: john
+```
+
+These are the available conditions for your rules:
+
+| Condition     | Possible values                                  |
+| ------------- | ------------------------------------------------ |
+| `if_type`     | `issue` or `pr`                                  |
+| `if_state`    | `open` or `closed`                               |
+| `if_created`  | any valid parameter for `moment().subtract(...)` |
+| `if_updated`  | any valid parameter for `moment().subtract(...)` |
+| `if_label`    | any label or `no`                                |
+| `if_assignee` | any username or `no`                             |
+| `if_comments` | number of comments (less or equal)               |
+
+And the available actions:
+
+| Action             | Possible values                                 |
+| ------------------ | ----------------------------------------------- |
+| `add_labels`       | label or array of labels                        |
+| `remove_labels`    | label or array of labels                        |
+| `replace_labels`   | label or array of labels                        |
+| `comment`          | comment body (see placeholders below)           |
+| `set_state`        | `open` or `closed`                              |
+| `set_locked`       | `false` or one of the valid reasons (see below) |
+| `add_assignees`    | username or array of usernames                  |
+| `remove_assignees` | username or array of usernames                  |
+
+The placeholder for your comments are:
+
+| Placeholder   | Values                                               |
+| ------------- | ---------------------------------------------------- |
+| `${AUTHOR}`   | Author of the issue, NOT including the leading `@`   |
+| `${ASSIGNEE}` | Assignee of the issue, NOT including the leading `@` |
+
+The leading `@` is NOT included in case you don't want to tag the user directly.
+
+Finally, the reasons to lock an issue are:
+`off-topic`, `too heated`, `resolved`, `spam`
+
+### Final word of advice from Ladybug
+
+The scheduled tasks are run every 5 minutes. Make sure you actions change the status of your issues in a way that the
+same action won't kick in again in 5 minutes, otherwise, you will create an infinite loop.
+
+That's all! Enjoy!
 
 ## Setup
 
@@ -29,8 +101,12 @@ timeouts:
 # Install dependencies
 yarn install
 
-# Run the bot
-yarn start
+# Run with hot reload
+yarn run build:watch
+
+# Compile and run
+yarn run build
+yarn run start
 ```
 
 ## Contributing
---
+++ src/index.ts
@@ -0,0 +1,367 @@
+import { Application, Context } from "probot";
+import createScheduler from "probot-scheduler";
+import metadata from "probot-metadata";
+import moment from "moment";
+
+interface AppConfig {
+  peer_labels?: boolean;
+  duplicated_issues?: false | DuplicatedIssuesConfig;
+  scheduled_checks?: ScheduledChecksConfig;
+}
+
+interface DuplicatedIssuesConfig {
+  label: string;
+  chain_close?: boolean;
+  chain_reopen?: boolean;
+}
+
+interface ScheduledChecksConfig {
+  [propName: string]: CheckConfig;
+}
+
+interface CheckConfig {
+  if_type?: "issue" | "pr";
+  if_state?: "open" | "closed";
+  if_created?: string;
+  if_updated?: string;
+  if_label?: "no" | string;
+  if_assignee?: "no" | string;
+  if_comments?: number;
+  add_labels?: string | string[];
+  remove_labels?: string | string[];
+  replace_labels?: string | string[];
+  comment?: string;
+  set_state?: "open" | "closed";
+  set_locked?: false | "off-topic" | "too heated" | "resolved" | "spam";
+  add_assignees?: string | string[];
+  remove_assignees?: string | string[];
+}
+
+const configPath = "ladybug.yml";
+const defaultConfig: AppConfig = {
+  peer_labels: true,
+  duplicated_issues: {
+    label: "Status: Duplicated",
+    chain_close: true,
+    chain_reopen: true,
+  },
+};
+
+export = (app: Application) => {
+  app.log("Ladybug on duty!");
+  createScheduler(app, {
+    interval: 5 * 60 * 1000, // 5 minutes
+  });
+
+  app.on("schedule.repository", checkScheduledTasks);
+  app.on("issues.labeled", removePeerLabels);
+  app.on("issues.closed", checkChainClosing);
+  app.on("issues.reopened", checkChainReopening);
+  app.on("issue_comment.created", checkDuplicateIssue);
+  app.on("issue_comment.edited", checkDuplicateIssue);
+  app.on("issue_comment.deleted", checkDuplicateIssue);
+
+  // Scheduled tasks
+  async function checkScheduledTasks(context: Context) {
+    const functions: Array<() => void> = [],
+      config: AppConfig = (await context.config(configPath, defaultConfig)) as AppConfig;
+
+    for (let checkName in config.scheduled_checks) {
+      const check = (config.scheduled_checks as ScheduledChecksConfig)[checkName];
+      app.log(`Processing task ${checkName}.`);
+
+      functions.push(async () => {
+        const { owner, repo } = context.repo(),
+          chunks: string[] = [`repo:${owner}/${repo}`];
+
+        if (check.if_type) {
+          chunks.push(`type:${check.if_type}`);
+        }
+        if (check.if_state) {
+          chunks.push(`state:${check.if_state}`);
+        }
+        if (check.if_created) {
+          chunks.push(
+            `created:<${moment()
+              .subtract(...check.if_created.split(" "))
+              .format(moment.defaultFormatUtc)}`
+          );
+        }
+        if (check.if_updated) {
+          chunks.push(
+            `updated:<${moment()
+              .subtract(...check.if_updated.split(" "))
+              .format(moment.defaultFormatUtc)}`
+          );
+        }
+        if (check.if_label) {
+          chunks.push(check.if_label == "no" ? `no:label` : `label:${check.if_label}`);
+        }
+        if (check.if_assignee) {
+          chunks.push(check.if_assignee == "no" ? `no:assignee` : `assignee:${check.if_assignee}`);
+        }
+        if (check.if_comments) {
+          chunks.push(`comments:<=${check.if_comments}`);
+        }
+
+        const q = chunks.join(" "),
+          searchResults = await context.github.search.issuesAndPullRequests({ q });
+        app.log(`Query: >>> ${q} <<< returned [${searchResults.data.items.map((x) => x.number).join(", ")}].`);
+
+        await Promise.all(
+          searchResults.data.items.map(async (issue) => {
+            if (check.remove_labels) {
+              const labels = issue.labels.map((x) => x.name),
+                targetLabels = Array.isArray(check.remove_labels) ? check.remove_labels : [check.remove_labels];
+              for (let label of targetLabels) {
+                if (!labels.includes(label)) {
+                  labels.push(label);
+                }
+              }
+              await context.github.issues.replaceLabels({
+                owner,
+                repo,
+                issue_number: issue.number,
+                labels: labels,
+              });
+            }
+            if (check.add_labels) {
+              await context.github.issues.addLabels({
+                owner,
+                repo,
+                issue_number: issue.number,
+                labels: Array.isArray(check.add_labels) ? check.add_labels : [check.add_labels],
+              });
+            }
+            if (check.replace_labels) {
+              await context.github.issues.replaceLabels({
+                owner,
+                repo,
+                issue_number: issue.number,
+                labels: Array.isArray(check.replace_labels) ? check.replace_labels : [check.replace_labels],
+              });
+            }
+            if (check.comment) {
+              const replacements = {
+                "${AUTHOR}": issue.user.login,
+                "${ASSIGNEE}": (issue.assignee as any)?.login || "", // assignee is considered null always, why?
+              };
+              let commentBody = check.comment;
+              for (let placeholder in replacements) {
+                commentBody = commentBody.split(placeholder).join(replacements[placeholder]);
+              }
+
+              await context.github.issues.createComment({
+                owner,
+                repo,
+                issue_number: issue.number,
+                body: commentBody,
+              });
+            }
+            if (check.set_state) {
+              await context.github.issues.update({
+                owner,
+                repo,
+                issue_number: issue.number,
+                state: check.set_state,
+              });
+            }
+            if (check.set_locked) {
+              if (typeof check.set_locked === "boolean") {
+                await context.github.issues.unlock({
+                  owner,
+                  repo,
+                  issue_number: issue.number,
+                });
+              } else {
+                await context.github.issues.lock({
+                  owner,
+                  repo,
+                  issue_number: issue.number,
+                  lock_reason: check.set_locked,
+                });
+              }
+            }
+            if (check.remove_assignees) {
+              await context.github.issues.removeAssignees({
+                owner,
+                repo,
+                issue_number: issue.number,
+                assignees: Array.isArray(check.remove_assignees) ? check.remove_assignees : [check.remove_assignees],
+              });
+            }
+            if (check.add_assignees) {
+              await context.github.issues.addAssignees({
+                owner,
+                repo,
+                issue_number: issue.number,
+                assignees: Array.isArray(check.add_assignees) ? check.add_assignees : [check.add_assignees],
+              });
+            }
+          })
+        );
+      });
+    }
+
+    await Promise.all(functions.map((x) => x()));
+  }
+
+  // Removes peer labels
+  async function removePeerLabels(context: Context) {
+    const config: AppConfig = (await context.config(configPath, defaultConfig)) as AppConfig,
+      { label, issue } = context.payload,
+      peerRegex = /^(.+:)/,
+      match = label.name.match(peerRegex);
+
+    if (match && config.peer_labels) {
+      const labels = issue.labels.filter((x) => !x.name.startsWith(match[1])).map((x) => x.name);
+      if (!labels.includes(label.name)) {
+        labels.push(label.name);
+      }
+      if (labels.length) {
+        await context.github.issues.replaceLabels(
+          context.issue({
+            labels,
+          })
+        );
+      }
+    }
+  }
+
+  // Checks created / edited / deleted issues for duplicate marking
+  async function checkDuplicateIssue(context: Context) {
+    const config: AppConfig = (await context.config(configPath, defaultConfig)) as AppConfig,
+      dupRegex = /^Duplicate of #(\d+)/,
+      { issue, comment, action, changes } = context.payload;
+
+    // Mark issue as duplicate
+    async function markDuplicate(duplicateOf: number) {
+      if ("label" in (config.duplicated_issues as DuplicatedIssuesConfig)) {
+        app.log(`Marking issue #${issue.number} as dup of #${duplicateOf}.`);
+        await metadata(context).set("duplicateOf", duplicateOf);
+        await context.github.issues.replaceLabels(
+          context.issue({
+            labels: [(config.duplicated_issues as DuplicatedIssuesConfig).label],
+          })
+        );
+      }
+    }
+
+    // Unmark issue as duplicate
+    async function unmarkDuplicate(duplicateOf: number) {
+      if ("label" in (config.duplicated_issues as DuplicatedIssuesConfig)) {
+        app.log(`Unmarking issue #${issue.number} as dup of #${duplicateOf}.`);
+        await metadata(context).set("duplicateOf", undefined);
+        await context.github.issues.removeLabel(
+          context.issue({
+            name: (config.duplicated_issues as DuplicatedIssuesConfig).label,
+          })
+        );
+      }
+    }
+
+    if (issue.state == "open" && action == "created") {
+      const match = comment.body.match(dupRegex);
+      if (match) {
+        markDuplicate(match[1]);
+      }
+    } else if (issue.state == "open" && action == "edited") {
+      const matchBefore = changes.body && changes.body.from.match(dupRegex),
+        matchAfter = comment.body.match(dupRegex);
+      if (matchAfter) {
+        markDuplicate(matchAfter[1]);
+      } else if (matchBefore) {
+        unmarkDuplicate(matchBefore[1]);
+      }
+    } else if (issue.state == "open" && action == "deleted") {
+      const match = comment.body.match(dupRegex);
+      if (match) {
+        unmarkDuplicate(match[1]);
+      }
+    }
+  }
+
+  // Checks closed issue for chain closing
+  async function checkChainClosing(context: Context) {
+    const config: AppConfig = (await context.config(configPath, defaultConfig)) as AppConfig,
+      { issue } = context.payload,
+      { owner, repo } = context.repo(),
+      q =
+        `is:issue is:open ` +
+        `label:"${(config.duplicated_issues as DuplicatedIssuesConfig).label}" ` +
+        `repo:${owner}/${repo}`,
+      searchResults = await context.github.search.issuesAndPullRequests({ q });
+
+    const chainClosed = (
+      await Promise.all(
+        searchResults.data.items.map(async (otherIssue) => {
+          otherIssue = context.repo(otherIssue);
+          const duplicateOf = await metadata(context, otherIssue).get("duplicateOf");
+
+          if (duplicateOf && issue.number == parseInt(duplicateOf)) {
+            app.log(`Chain closing issue #${otherIssue.number}.`);
+            await context.github.issues.update({
+              owner,
+              repo,
+              issue_number: otherIssue.number,
+              state: "closed",
+            });
+            return `#${otherIssue.number}`;
+          }
+          return null;
+        })
+      )
+    ).filter((x) => x);
+
+    if (chainClosed.length) {
+      await context.github.issues.createComment(
+        context.issue({
+          body: `Closed other issues that were marked as duplicates of this one: ${chainClosed.join(", ")}`,
+        })
+      );
+    }
+  }
+
+  // Checks reopened issue for chain reopening
+  async function checkChainReopening(context: Context) {
+    const config: AppConfig = (await context.config(configPath, defaultConfig)) as AppConfig;
+    if ("label" in (config.duplicated_issues as DuplicatedIssuesConfig)) {
+      const { issue } = context.payload,
+        { owner, repo } = context.repo(),
+        q =
+          `is:issue is:closed ` +
+          `label:"${(config.duplicated_issues as DuplicatedIssuesConfig).label}" ` +
+          `repo:${owner}/${repo}`,
+        searchResults = await context.github.search.issuesAndPullRequests({ q });
+
+      const chainReopened = (
+        await Promise.all(
+          searchResults.data.items.map(async (otherIssue) => {
+            otherIssue = context.repo(otherIssue);
+            const duplicateOf = await metadata(context, otherIssue).get("duplicateOf");
+
+            if (duplicateOf && issue.number == parseInt(duplicateOf)) {
+              app.log(`Chain reopening issue #${otherIssue.number}.`);
+              await context.github.issues.update({
+                owner,
+                repo,
+                issue_number: otherIssue.number,
+                state: "open",
+              });
+              return `#${otherIssue.number}`;
+            }
+            return null;
+          })
+        )
+      ).filter((x) => x);
+
+      if (chainReopened.length) {
+        await context.github.issues.createComment(
+          context.issue({
+            body: `Reopened other issues that were marked as duplicates of this one: ${chainReopened.join(", ")}`,
+          })
+        );
+      }
+    }
+  }
+};
